336b5f4491360edb568186fad299e086940929c0
Acidiney Dias <acidineydias@gmail.com>
2022-03-08 18:17:46 +0100

    feat: ensure AccountMongoRepository returns an account on success

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,12 +1,14 @@
-import { MongoClient } from 'mongodb'
+import { MongoClient, Collection } from 'mongodb'
 
 export const MongoHelper = {
   client: null as MongoClient,
   async connect (url: string): Promise<void> {
-    this.client = new MongoClient(url)
-    await this.client.connect()
+    this.client = await MongoClient.connect(url)
   },
   async disconnect (): Promise<void> {
     await this.client.close()
+  },
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
   }
 }
