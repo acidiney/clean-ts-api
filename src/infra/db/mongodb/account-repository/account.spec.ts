4483bd2f5530b719d7e7afb243692d50a870b2d2
Acidiney Dias <acidineydias@gmail.com>
2022-03-08 18:22:52 +0100

    refactor: clear database between tests

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -15,6 +15,12 @@ describe('Account Mongo Repository', () => {
     await MongoHelper.disconnect()
   })
 
+  beforeEach(async () => {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+
+    await accountCollection.deleteMany({})
+  })
+
   test('should returns an account on success', async () => {
     const sut = makeSut()
     const account = await sut.add({
