7e0bc04bf8e6523be15f5b104e51605a4d222e6c
Acidiney Dias <acidineydias@gmail.com>
2022-03-08 14:37:02 +0100

    test: ensure BcryptAdapter throws if bcrypt throws

--- src/infra/cryptography/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -31,4 +31,17 @@ describe('Bcrypt Adapter', () => {
 
     expect(hashedValue).toBe('hash')
   })
+
+  test('should throws if bcrypt throws', async () => {
+    const sut = makeSut()
+
+    jest.spyOn(bcrypt, 'hash')
+      .mockImplementationOnce(() => {
+        throw new Error()
+      })
+
+    const promise = sut.encrypt('any_value')
+
+    await expect(promise).rejects.toThrow()
+  })
 })
