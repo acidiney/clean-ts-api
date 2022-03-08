c4e7a085651259b5d2080650dc184361870cbf6c
Acidiney Dias <acidineydias@gmail.com>
2022-03-08 19:59:18 +0100

    feat: add signup factory

---
+++ src/main/factories/signup.ts
@@ -0,0 +1,25 @@
+import { AddAccount } from '../../domain/usecases/add-account'
+import { BcryptAdapter } from '../../infra/cryptography/bcrypt-adapter'
+import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
+import { EmailValidator } from '../../presentation/protocols/email-validator'
+import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
+import { SignUpController } from '../../presentation/controllers/signup/signup'
+import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+
+const makeAddAccount = (): AddAccount => {
+  const salt = 12
+
+  const encrypter = new BcryptAdapter(salt)
+  const addAccountRepository = new AccountMongoRepository()
+  return new DbAddAccount(encrypter, addAccountRepository)
+}
+
+const makeEmailValidatorAdapter = (): EmailValidator => {
+  return new EmailValidatorAdapter()
+}
+
+export const makeSignUpController = (): SignUpController => {
+  const addAccount = makeAddAccount()
+  const emailValidatorAdapter = makeEmailValidatorAdapter()
+  return new SignUpController(addAccount, emailValidatorAdapter)
+}
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,5 +1,20 @@
-import { app } from './config/app'
+import { config } from 'dotenv'
+import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
 
-app.listen(5050, () => {
-  console.log('Server running at http://localhost:5050')
-})
+config()
+
+async function main (): Promise<void> {
+  const { default: env } = await import('./config/env')
+
+  MongoHelper.connect(env.mongoUrl)
+    .then(async () => {
+      const { app } = await import('./config/app')
+      app.listen(env.port, () => {
+        console.log('Server running at http://localhost:5050')
+      })
+    })
+    .catch(console.error)
+}
+
+main()
+  .catch(p => p)
