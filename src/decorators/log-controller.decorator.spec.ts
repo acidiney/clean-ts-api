3dab2b3a458258c6eb29723fa22ea13152446b05
Acidiney Dias <acidineydias@gmail.com>
2022-03-09 22:45:39 +0100

    test: ensure LogControllerDecorator returns the same result of the controller

--- src/decorators/log-controller.decorator.spec.ts
+++ src/decorators/log-controller.decorator.spec.ts
@@ -49,5 +49,25 @@ describe('LogControllerDecorator', function () {
     expect(handleSpy).toHaveBeenCalledWith(httpRequest)
   })
 
-  // shoul')
+  it('should return  the same result of the controller', async () => {
+    const { sut } = makeSut()
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_mail@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse).toEqual({
+      body: {
+        name: 'any_name'
+      },
+      statusCode: 200
+    })
+  })
 })
